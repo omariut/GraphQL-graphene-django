039778ea4e727417cadca5814a9ccd3467f13074
omariut <omarcpgcbl@gmail.com>
2022-12-30 22:47:43 +0600

    basic tutorial

---
+++ ingredients/admin.py
@@ -0,0 +1,6 @@
+# cookbook/ingredients/admin.py
+from django.contrib import admin
+from ingredients.models import Category, Ingredient
+
+admin.site.register(Category)
+admin.site.register(Ingredient)
